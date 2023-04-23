c1ac14255c1c960588036f8ca0d4abde0e5c39e9
Manoj-t <teluguntlamanoj@gmail.com>
2023-04-23 14:00:44 -0500

    added RepositoryRestResource annotation and property for default size in application.properties

--- src/main/java/com/manoj/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/manoj/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.manoj.springboot.cruddemo.dao;
 
 import com.manoj.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it
+    // that's it ... no need to write any code LOL!
 }
